b18eb427493f0c1bf095fee8b6c46af8bebe8385
Mikle <zimboroda@gmail.com>
2019-12-10 17:19:16 +0200

    My template  MQsort

--- postcss.config.js
+++ postcss.config.js
@@ -3,21 +3,23 @@
 // cssnano      - https://github.com/hail2u/node-css-mqpacker
 
 // npm install postcss-loader autoprefixer css-mqpacker cssnano --save-dev
+const sortCSSmq = require('sort-css-media-queries');
+
 
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('css-mqpacker')({
-      sort: true
-    }),
-    require('cssnano')({
-      preset: [
-        'default', {
-          discardComments: {
-            removeAll: true
-          }
-        }
-      ]
-    })
-  ]
+    plugins: [
+        require('autoprefixer'),
+        require('css-mqpacker')({
+            sort: sortCSSmq
+        }),
+        require('cssnano')({
+            preset: [
+                'default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }
+            ]
+        })
+    ]
 }
