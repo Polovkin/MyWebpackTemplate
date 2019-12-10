ac203b87bf6c8375d8e5b3a2d9dac5e62641424d
Mikle <zimboroda@gmail.com>
2019-12-10 11:33:16 +0200

    My template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 // JS
-import './js/ajax'
-import './js/main'
+
 
 // CSS (example)
 
 // SCSS
-import './assets/css/fontstyles.css'
-import './assets/scss/main.scss'
+
 // import './assets/css/main.css'
 
 
