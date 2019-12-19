264a5bc44e0641fcf0d5e43f7c57812d093772de
Polovkin <zimboroda@gmail.com>
2019-12-19 22:59:44 +0200

    Build without index.html

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -195,7 +195,7 @@ module.exports = {
         new CopyWebpackPlugin([
             //{from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img`},
             //{from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
-            {from: `${PATHS.src}/static`, to: ''},
+           // {from: `${PATHS.src}/static`, to: ''},
         ]),
 
         // Automatic creation any html pages (Don't forget to RERUN dev server)
