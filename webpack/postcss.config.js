1f9733dad66c61c93aa3a47cd7489de7bf806471
Polovkin <zimboroda@gmail.com>
2020-02-29 13:55:00 +0200

    ColorGuard add

--- webpack/postcss.config.js
+++ webpack/postcss.config.js
@@ -1,5 +1,5 @@
 const sortCSSmq = require('sort-css-media-queries');
-// const colors = require('colors');
+const colors = require('colors');
 
 module.exports = {
   plugins: [
@@ -17,26 +17,26 @@ module.exports = {
       ],
     }),
     require('colorguard'),
+    // require('doiuse')({
+    //   ignore: ['rem'],
+    //   ignoreFiles: ['**/normalize.css'],
+    //   // onFeatureUsage: function(usageInfo) { }
+    //   onFeatureUsage(info) {
+    //     const selector = info.usage.parent.selector;
+    //     const property = `${info.usage.prop}: ${info.usage.value}`;
+    //
+    //     let status = info.featureData.caniuseData.status.toUpperCase();
+    //
+    //     if (info.featureData.missing) {
+    //       status = 'NOT SUPPORTED'.red;
+    //     } else if (info.featureData.partial) {
+    //       status = 'PARTIAL SUPPORT'.yellow;
+    //     }
+    //
+    //     console.log(`\n${status}:\n\n ${selector} {\n ${property};\n }\n`);
+    //   },
+    // }),
   ],
 };
 
 
-// require('doiuse')({
-//   ignore: ['rem'],
-//   ignoreFiles: ['**/normalize.css'],
-//   //onFeatureUsage: function(usageInfo) { }
-//   onFeatureUsage(info) {
-//     const selector = info.usage.parent.selector;
-//     const property = `${info.usage.prop}: ${info.usage.value}`;
-//
-//     let status = info.featureData.caniuseData.status.toUpperCase();
-//
-//     if (info.featureData.missing) {
-//       status = 'NOT SUPPORTED'.red;
-//     } else if (info.featureData.partial) {
-//       status = 'PARTIAL SUPPORT'.yellow;
-//     }
-//
-//     console.log(`\n${status}:\n\n    ${selector} {\n        ${property};\n    }\n`);
-//   }
-// }),
