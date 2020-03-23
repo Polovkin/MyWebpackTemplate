31143bf2968118893e655c40baaeb0f29076e714
Polovkin <zimboroda@gmail.com>
2020-03-23 21:46:21 +0200

    Babel polifil add

--- src/vue/vue-entry.js
+++ src/vue/vue-entry.js
@@ -1,12 +1,11 @@
 import '@babel/polyfill';
 import Vue from 'vue';
-import App from "./App.vue";
+import App from './App.vue';
 Vue.config.productionTip = false;
 
 Vue.component('example-component', require('./components/Example.vue').default);
 Vue.component('primary-button', require('./components/PrimaryButton.vue').default);
 
-new Vue({render: h => h(App)}).$mount("#app");
-
+new Vue({render: (h) => h(App)}).$mount('#app');
 
 
