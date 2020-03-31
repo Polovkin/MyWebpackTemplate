147ba044960ed161fb53c039d4ec21eb81b712c3
Polovkin <zimboroda@gmail.com>
2020-03-31 18:21:44 +0300

    Picture test

--- src/js/utils/lazyloading.js
+++ src/js/utils/lazyloading.js
@@ -1,4 +1,4 @@
-//Lazy-loading
+// Lazy-loading
 document.addEventListener('DOMContentLoaded', function() {
   let lazyLoadImages;
 
@@ -48,31 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('orientationChange', lazyLoad);
   }
 });
-
-//Smoth paga scroll
-let $page = $('html, body');
-$('a[href*="#"]').click(function () {
-  $page.animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
-  return false;
-});
-
-//Click on header mobile size)
-$(document).mouseup(function(e) {
-  const container = $('#header');
-  if (container.has(e.target).length === 0) {
-    $('.header__nav').removeClass('show');
-  }
-});
-
-//Video auto height
-const iframe = $('.main-video-popup iframe');
-const width = iframe.width();
-iframe.css('height', width / 1.7777 + 'px');
-
-
-
 // Css trik
 // #bg-image.lazy {
 //   background-image: none;
--- webpack/index.js
+++ webpack/index.js
@@ -1,16 +1,17 @@
 
 
 // JS
-import '../src/js/custom';
-// import '../src/js/base';
-// import 'bootstrap';
+import '../src/js/scripts/custom';
 
+import '../src/js/utils/base';
+import '../src/js/utils/lazyloading';
+// import 'bootstrap';
 
 
 // SCSS
 import '../src/assets/scss/main.scss';
 // Vue.js
-import '../src/vue/vue-entry';
+// import '../src/vue/vue-entry';
 // CSS
 import '@fortawesome/fontawesome-free/css/all.css';
 
