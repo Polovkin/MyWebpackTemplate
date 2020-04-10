13c9fff5030b4eea7a93ad4fc48cb8d109572f06
Polovkin <zimboroda@gmail.com>
2020-04-10 18:41:04 +0300

    typography add

--- webpack/index.js
+++ webpack/index.js
@@ -5,14 +5,14 @@ import '../src/js/scripts/custom';
 
 import '../src/js/utils/base';
 import '../src/js/utils/lazyloading';
-//import 'bootstrap';
+// import 'bootstrap';
 
 // TS
 
 // SCSS
 import '../src/assets/scss/main.scss';
 // Vue.js
- //import '../src/vue/vue-entry';
+// import '../src/vue/vue-entry';
 // CSS
 import '@fortawesome/fontawesome-free/css/all.css';
 
