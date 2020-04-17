4b0197031d4d5cde37b57ef0f309ce6bb2676a22
Polovkin <zimboroda@gmail.com>
2020-04-17 10:47:08 +0300

    Img mixin

--- webpack/index.js
+++ webpack/index.js
@@ -6,16 +6,17 @@ import '@/js/scripts/custom';
 import '@/js/utils/base';
 import '@/js/utils/lazyloading';
 import '@/js/components/accordion';
-//import 'bootstrap';
+import 'bootstrap';
 
 // TS
 
 // SCSS
 import '@/assets/scss/main.scss';
 // Vue.js
-import '@/vue/vue-entry';
+//import '@/vue/vue-entry';
 // CSS
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
