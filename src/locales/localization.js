f7e122b58bbbec873dcc5c6bcb30887291f06545
Polovkin <zimboroda@gmail.com>
2020-06-08 20:27:43 +0300

    Pug localization

--- src/locales/localization.js
+++ src/locales/localization.js
@@ -1,34 +1,34 @@
-import ru from '../locales/ru.json'
-import en from '../locales/en.json'
-
-const locales = {
-  'ru': ru,
-  'en': en,
-}
-
-function setLocale() {
-  let localeElements = $('[data-locale]');
-  let currentLocale = localStorage.getItem('lang') ?
-    JSON.parse(localStorage.getItem('lang')) :
-    document.documentElement.lang
-
-
-  localeElements.each(function () {
-    const $this = $(this)
-    let key = $this.data().locale;
-    if (this.nodeName !== 'IMG') {
-      $this.text(locales[currentLocale][key]);
-    } else {
-      $this.attr('src', '/assets/img/' + locales[currentLocale].img[key]);
-    }
-  })
-};
-
-$(document).ready(function () {
-  setLocale()
-  $('#lang button').click(function () {
-    document.documentElement.lang = $(this).text();
-    localStorage.setItem('lang', JSON.stringify($(this).text()));
-    setLocale();
-  });
-});
+// import ru from '../locales/ru.json'
+// import en from '../locales/en.json'
+//
+// const locales = {
+//   'ru': ru,
+//   'en': en,
+// }
+// console.log(locales);
+//
+// function setLocale() {
+//   let currentLocale = localStorage.getItem('lang') ?
+//     JSON.parse(localStorage.getItem('lang')) :
+//     document.documentElement.lang
+//
+//
+//   $('[data-locale]').each(function () {
+//     const $this = $(this)
+//     let key = $this.data().locale;
+//     if (this.nodeName !== 'IMG') {
+//       $this.text(locales[currentLocale][key]);
+//     } else {
+//       $this.attr('src', '/assets/img/' + locales[currentLocale].img[key]);
+//     }
+//   })
+// };
+//
+// $(document).ready(function () {
+//   setLocale()
+//   $('#lang button').click(function () {
+//     document.documentElement.lang = $(this).text();
+//     localStorage.setItem('lang', JSON.stringify($(this).text()));
+//     setLocale();
+//   });
+// });
