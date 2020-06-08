3d2286e24204a41da478a5ece4b98571c294783c
Polovkin <zimboroda@gmail.com>
2020-06-08 18:23:51 +0300

    JS singlepage localization

--- src/locales/localization.js
+++ src/locales/localization.js
@@ -5,14 +5,22 @@ const locales = {
   'ru': ru,
   'en': en,
 }
+
 function setLocale() {
   let localeElements = $('[data-locale]');
+  let currentLocale = localStorage.getItem('lang') ?
+    JSON.parse(localStorage.getItem('lang')) :
+    document.documentElement.lang
 
-  let currentLocale = document.documentElement.lang
+
   localeElements.each(function () {
     const $this = $(this)
     let key = $this.data().locale;
-    $this.text(locales[currentLocale][key]);
+    if (this.nodeName !== 'IMG') {
+      $this.text(locales[currentLocale][key]);
+    } else {
+      $this.attr('src', '/assets/img/' + locales[currentLocale].img[key]);
+    }
   })
 };
 
@@ -20,6 +28,7 @@ $(document).ready(function () {
   setLocale()
   $('#lang button').click(function () {
     document.documentElement.lang = $(this).text();
+    localStorage.setItem('lang', JSON.stringify($(this).text()));
     setLocale();
   });
 });
