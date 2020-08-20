171b94c76d6ab72ac76d8d64ac3203bc5160de88
Polovkin <zimboroda@gmail.com>
2020-08-20 08:20:53 +0300

    select

--- webpack/index.js
+++ webpack/index.js
@@ -1,12 +1,13 @@
 // JS
 //import '../src/js/utils/base';
-import '../src/js/utils/lazyloading';
+//import '../src/js/utils/lazyloading';
 
 //import '../src/js/scripts/custom';
 //import '../src/js/components/test';
 //import '../src/js/project/widget/forms'
 //import '../src/js/project/layouts/header'
 //import '../src/js/project/modules/slider'
+import '../src/js/plugins/select'
 
 
 //import '../src/locales/localization.js'
@@ -20,7 +21,7 @@ import '../src/js/utils/lazyloading';
 import '@/assets/scss/main.scss';
 
 // Vue.js
-import '@/vue/vue-entry';
+//import '@/vue/vue-entry';
 // CSS
 
 
