94a9405ba17c2e91bf80c9671a04690c8f06f192
Polovkin <zimboroda@gmail.com>
2020-09-15 11:38:48 +0300

    html config fix

--- webpack/index.js
+++ webpack/index.js
@@ -1,19 +1,4 @@
 // JS
-//import '../src/js/utils/base';
-//import '../src/js/utils/lazyloading';
-
-//import '../src/js/scripts/custom';
-//import '../src/js/components/test';
-import '@/js/project/modules/forms'
-//import '../src/js/project/layouts/header'
-//import '../src/js/project/modules/slider'
-import '../src/js/plugins/select'
-
-
-//import '../src/locales/localization.js'
-//import '../src/locales/localization.js'
-
-
 
 // TS
 
@@ -27,14 +12,4 @@ import '@/vue/vue-entry';
 
 //Vendors
 import 'normalize.css';
-
-
-
-// import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
-// import '@fortawesome/fontawesome-free/js/all'
-
-//import 'imagehover.css/css/imagehover.css';
-
-//import 'bootstrap';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
